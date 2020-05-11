5f74a90122e7d7725ef123578a2c6d98716a19ec
Ian Parberry <Ian-Parberry@users.noreply.github.com>
2020-05-11 14:48:31 -0500

    Refresh documentation.

--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['togglelinethickness',['ToggleLineThickness',['../class_c_main.html#a10cc16a2d76eb2577d8767911411f4dc',1,'CMain']]],
   ['toggleshowrules',['ToggleShowRules',['../class_c_main.html#a286d71b63234e7faa1bb40e37a6e4bba',1,'CMain']]],
-  ['turtledesc',['TurtleDesc',['../class_turtle_desc.html#ab3cfdae8d0476a056897c9e5a55f40c0',1,'TurtleDesc::TurtleDesc()'],['../class_turtle_desc.html#a1015ba0569afe744c02d39bdadf321ce',1,'TurtleDesc::TurtleDesc(float angledelta, float len)']]]
+  ['turtledesc',['TurtleDesc',['../class_turtle_desc.html#a1015ba0569afe744c02d39bdadf321ce',1,'TurtleDesc']]]
 ];
